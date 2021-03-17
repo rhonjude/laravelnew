aa0e95d5626d1aeaf5422b7df0cbcdefe088fe7b
rhonjude <rhonjude007@gmail.com>
2021-03-17 11:55:54 +0530

    stud

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+  
+  <div class="row">
+  <div class="col col-12 col-md-4 col-lg-4 col-xxl-4"></div>
+  <div class="col col-12 col-md-4 col-lg-4 col-xxl-4">
+  <table class="table table-borderless">
+  <tr>
+      <td>name</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td>roll.no</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td>admno</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td>college</td>
+      <td><input type="text" class="form-control"></td>
+  </tr>
+  <tr>
+      <td></td>
+      <td><button class="btn btn-danger">Submit</button></td>
+  </tr>
+  </table></div></div>
+  </div>  
+</body>
+</html>
